997e88286f3c57199b260ff7b764fcd6e8544cfb
George Phillips <georgephillips22@gmail.com>
2024-11-17 22:57:15 +0000

    Updated Readme & Dependency checks

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-# gram - README.md
+# gram
 
-Actually, I've decided README.md is great.
+The idea of gram is to design an application, around user identities, interfacing over a single datascape.
 
+gram, is a server that serves itself, and the files that are needed to build it.
 
-This is an example of a self hosted, modules, with universal execution capabilities
+I plan to build out to adding peer & data, connectity and ownership, and then maybe into a more distributed system.
+
+## What?
+
+In short I want to a build super simple universal executable environment, communicating over a single dataspace. Via distributed content addressed data hashing.
+
+Universal modules.
+- Interoperability (Wit IDL), for most major language, with any language being able to create bindings
+- Execution, through wasm, with many major languages being able to compile to wasm. Runs in browsers.
+- Standardised: Map HTTP, to POSIX.
+- Component Model: Wasi's component model, maps well to the directory design of a POSIX filesystem.
+
+Next:
+- [ ] Implement process networking
+- [ ] Implement users
+- [ ] Implement datastore
+- [ ] Implement confidence scores
+
--- src/lib.rs
+++ src/lib.rs
@@ -10,30 +10,13 @@ struct Component;
 
 bindings::export!(Component with_types_in bindings);
 
-fn print_dir() {
-    let dir_path = "."; // Current directory in the sandbox
-
-    // Read the directory
-    let entries = fs::read_dir(dir_path).unwrap();
-
-    println!("Files and directories in '{}':", dir_path);
-
-    for entry in entries {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        println!("{}", path.display());
-    }
-}
-
-fn find_entry(path: Arc<String>) -> Vec<u8> {
-    // Get the directory of the executable
-    // Open and read the file at the path, returning the contents
-    println!("Reading  {path:?}");
+/**
+ * read the file at the given path and return the contents as a Vec<u8>
+ */
+fn read_file(path: Arc<String>) -> Vec<u8> {
+    println!("Reading {path:?}");
     match fs::read(&*path) {
-        Ok(contents) => {
-            // Return the executable contents
-            contents
-        }
+        Ok(contents) => contents,
         Err(e) => format!("Failed to read executable: {e:?}").into_bytes(),
     }
 }
@@ -49,11 +32,10 @@ impl bindings::exports::wasi::http::incoming_handler::Guest for Component {
         ResponseOutparam::set(outparam, Ok(resp));
 
         let out = body.write().expect("outgoing stream");
-        let data = find_entry(Arc::new(
+        let data = read_file(Arc::new(
             "./target/wasm32-wasip1/debug/gram.wasm".to_string(),
         ));
         println!("Writing response");
-        // println!("{:?}", data);
         for chunk in data.chunks(4096) {
             if let Err(e) = out.blocking_write_and_flush(chunk) {
                 eprintln!("Failed to write response chunk: {:?}", e);
@@ -62,5 +44,20 @@ impl bindings::exports::wasi::http::incoming_handler::Guest for Component {
         }
         drop(out);
         OutgoingBody::finish(body, None).unwrap();
+    }
+}
+
+fn print_dir() {
+    let dir_path = ".";
+
+    // Read the directory
+    let entries = fs::read_dir(dir_path).unwrap();
+
+    println!("Files and directories in '{}':", dir_path);
+
+    for entry in entries {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        println!("{}", path.display());
     }
 }
